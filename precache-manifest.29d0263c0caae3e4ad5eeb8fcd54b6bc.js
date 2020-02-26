39682e3d95534f4ab5fb2ba67ecd259debe1eced
thomaswong1000@gmail.com <twong40@ucsc.edu>
2020-02-25 22:56:48 -0800

    Updates

---
+++ precache-manifest.29d0263c0caae3e4ad5eeb8fcd54b6bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78b9ba5816ab196d35daf253c40ea25e",
+    "url": "/ExampleWeatherApp/index.html"
+  },
+  {
+    "revision": "02dc93398c4227ecb585",
+    "url": "/ExampleWeatherApp/static/css/main.a1d16987.chunk.css"
+  },
+  {
+    "revision": "95cca85792093e9e41e4",
+    "url": "/ExampleWeatherApp/static/js/2.b48c76c1.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/ExampleWeatherApp/static/js/2.b48c76c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02dc93398c4227ecb585",
+    "url": "/ExampleWeatherApp/static/js/main.5e9c1455.chunk.js"
+  },
+  {
+    "revision": "9a5dc6e0adb721074828",
+    "url": "/ExampleWeatherApp/static/js/runtime-main.30edab64.js"
+  }
+]);
